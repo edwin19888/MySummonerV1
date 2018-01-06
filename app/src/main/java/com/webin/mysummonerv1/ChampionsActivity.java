c77d7c90133bf074590d6133e26b36d0f5309948
edwin19888 <edwin19888@gmail.com>
2018-01-05 22:03:56 -0500

    Actualizado - 05/01/2018 - 09:59pm

---
+++ app/src/main/java/com/webin/mysummonerv1/ChampionsActivity.java
@@ -0,0 +1,23 @@
+package com.webin.mysummonerv1;
+
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
+
+public class ChampionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_champions);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);//no girar activity
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+    }
+
+}
