e3b6545444bc583c3e46dc94ba3f49c43ef10975
edwin19888 <edwin19888@gmail.com>
2018-01-05 21:48:46 -0500

    Actualizado - 05/01/2018 - 09:46pm

--- app/src/main/java/com/webin/mysummonerv1/MainActivity.java
+++ app/src/main/java/com/webin/mysummonerv1/MainActivity.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.app.FragmentManager;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -23,6 +26,7 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -30,16 +34,38 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity implements AlertDialogRadio.AlertPositiveListener,AlertDialogRadio.AlertNegativeListener {
 
     EditText editTextSummoner;
+    ImageView imageViewChampion;
 
     CharSequence[] values = {"Korea","Lationamerica Norte","Lationamerica Sur","Brazil","North America"};
     int position = 0;
+    int mostrarServer;
+
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
+        SharedPreferences prefs = getSharedPreferences("MisPreferencias",Context.MODE_PRIVATE);
+        int mostrarServer = prefs.getInt("mostrarServer", 0);
+        String serverName = prefs.getString("serverName","Korea");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);//no girar activity
 
-        openDialogServer();
+
+        imageViewChampion = (ImageView) findViewById(R.id.ivChampions);
+        imageViewChampion.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,ChampionsActivity.class);
+                startActivity(i);
+            }
+        });
+
+        if (mostrarServer == 0) {
+            openDialogServer();
+        }
 
         final String[] summoner = new String[1];
 
@@ -50,9 +76,10 @@ public class MainActivity extends AppCompatActivity implements AlertDialogRadio.
 
                 if  ((actionId == EditorInfo.IME_ACTION_SEARCH)) {
 
+
                     summoner[0] = editTextSummoner.getText().toString();
                     if(summoner[0].length() > 0){
-                        Mensaje("Usuario a buscar : " + summoner[0]);
+                        Mensaje(summoner[0]);
                     }else{
                         Mensaje("Debe ingresar un usuario");
                     }
@@ -95,10 +122,19 @@ public class MainActivity extends AppCompatActivity implements AlertDialogRadio.
         this.position = position;
 
         /** Getting the reference of the textview from the main layout */
+        String serverSelection = Android.code[this.position];
 
         Toast.makeText(MainActivity.this,"Check: "+Android.code[this.position],Toast.LENGTH_SHORT).show();
         /** Setting the selected android version in the textview */
         //tv.setText("Your Choice : " + Android.code[this.position]);
+
+        SharedPreferences prefs =
+                getSharedPreferences("MisPreferencias",Context.MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("serverName", serverSelection);
+        editor.putInt("mostrarServer", 1);
+        editor.commit();
     }
 
     @Override
@@ -109,5 +145,12 @@ public class MainActivity extends AppCompatActivity implements AlertDialogRadio.
         Toast.makeText(MainActivity.this,"Check: "+Android.code[this.position],Toast.LENGTH_SHORT).show();
         /** Setting the selected android version in the textview */
         //tv.setText("Your Choice : " + Android.code[this.position]);
+        String serverSelection = "Korea";
+        SharedPreferences prefs =
+                getSharedPreferences("MisPreferencias",Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("serverName", serverSelection);
+        editor.putInt("mostrarServer", 1);
+        editor.commit();
     }
 }
